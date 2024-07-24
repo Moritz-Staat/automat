136d9f8d8efef41194b34d520a8eebda23f6c6b0
Moritz Staat <moritz@moritz-staat.de>
2024-07-24 13:49:43 +0200

    Zwischensichern

---
+++ site/Einzelseiten/javalevel1.js
@@ -0,0 +1,39 @@
+document.getElementById('logo').onclick = function() {
+    document.getElementById('pinModal').style.display = 'block';
+}
+
+document.getElementsByClassName('close')[0].onclick = function() {
+    closeModal();
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('pinModal')) {
+        closeModal();
+    }
+}
+
+document.getElementById('submitPin').onclick = function() {
+    const pinInput = document.getElementById('pinInput');
+    if (pinInput.value === '151107') {
+        alert('PIN korrekt! Relais wird ausgelöst.');
+        doPost('1', 'http://192.168.0.120/Start');
+        closeModal();
+    } else {
+        pinInput.classList.add('error');
+        setTimeout(() => {
+            pinInput.classList.remove('error');
+            closeModal();
+        }, 1000);
+    }
+}
+
+function doPost(param, url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url + "?param=" + param, true);
+    xhr.send();
+}
+
+function closeModal() {
+    document.getElementById('pinInput').value = '';
+    document.getElementById('pinModal').style.display = 'none';
+}

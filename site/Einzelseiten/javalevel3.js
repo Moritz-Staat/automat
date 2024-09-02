eb3075536f47910e00806a770cac6d24d8f1a960
Moritz <moritz@moritz-staat.de>
2024-09-02 18:39:43 +0200

    Level 3

--- site/Einzelseiten/javalevel3.js
+++ site/Einzelseiten/javalevel3.js
@@ -52,6 +52,7 @@ function closeModal() {
 let timeout = undefined
 /* Nach Beenden des Quizzes Zurück Verlinken zur Startseite*/
 window.addEventListener('message', (event) => {
+    /*
     if (timeout != undefined) {
         return
     }
@@ -83,10 +84,65 @@ window.addEventListener('message', (event) => {
             timeout = undefined
             window.location.href = '../Automat.html';
         }, 3000);
-    }
+    }*/
+    show(event.data)
 });
 function doPost(param, url) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", url + "?param=" + param, true);
     xhr.send();
 }
+let _data;
+function show(data) {
+    document.getElementById('Preisauswahl').classList.remove('preise');
+    _data=data;
+    document.getElementById('Preisauswahl').classList.add('preiseshown');
+}
+
+premiumButton.addEventListener('click', () => {
+    // Ändere den Popup-Inhalt für das Premium-Angebot
+    popupText.textContent = "Melde dich bei unserem Stand und wir geben dir dein Premium Geschenk";
+    popupGif.style.display = 'none';
+    popupButtons.style.display = 'none';
+    timeout = setTimeout(() => {
+        timeout = undefined
+        window.location.href = '../Automat.html';
+    }, 3000);
+});
+
+normalButton.addEventListener('click', () => {
+    if (timeout != undefined) {
+        return
+    }
+    if (_data === 'prizeCollected') {
+        let level3wins = localStorage.getItem('level3win')
+        if (level3wins == null) {
+            level3wins = 1
+        } else {
+            level3wins = parseInt(level3wins)
+            level3wins += 1;
+        }
+        localStorage.setItem('level3win', level3wins.toString())
+        doPost('1', 'http://192.168.0.120/Beginner');
+        timeout = setTimeout(() => {
+            timeout = undefined
+            window.location.href = '../Automat.html';
+        }, 3000);
+    } else if (_data === 'quizFailed') {
+        let loses = localStorage.getItem('loses')
+        if (loses == null) {
+            loses = 1
+        } else {
+            loses = parseInt(loses)
+            loses += 1;
+        }
+        localStorage.setItem('loses', loses.toString())
+        doPost('1', 'http://192.168.0.120/Trostpreis');
+        timeout = setTimeout(() => {
+            timeout = undefined
+            window.location.href = '../Automat.html';
+        }, 300);
+    }
+    popup.style.display = 'none';
+    mainContent.classList.remove('blurred');
+});

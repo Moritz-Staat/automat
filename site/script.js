b6cfcd294b4e684af8b3f7dc96f230f21fc0708d
Moritz <moritz@moritz-staat.de>
2024-09-17 12:25:37 +0200

    new h2 and h1 on Automat.html

--- site/script.js
+++ site/script.js
@@ -126,3 +126,25 @@ function birnenwechsler() {
     }
 }
 birnenwechsler()
+
+// Überschrift Animation
+
+window.addEventListener('load', () => {
+    const h1 = document.getElementById('animated-h1');
+
+    // Funktion zum Ausführen der Animation
+    const animateHeading = () => {
+        h1.classList.add('animate');
+
+        // Entferne die Klasse nach der Dauer der Animation (2 Sekunden), um sie erneut starten zu können
+        setTimeout(() => {
+            h1.classList.remove('animate');
+        }, 2000); // 2 Sekunden entspricht der Animationsdauer in CSS
+    };
+
+    // Starte die Animation beim ersten Laden
+    animateHeading();
+
+    // Wiederhole die Animation alle 10 Sekunden
+    setInterval(animateHeading, 10000); // 10.000 Millisekunden = 10 Sekunden
+});
